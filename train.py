3ad35631bb36db7d8583c443cf98475802fbb6a3
wegamekinglc <scrappedprince.li@gmail.com>
2020-06-30 17:46:38 +0800

    FEATURE: use distributed training

--- train.py
+++ train.py
@@ -18,74 +18,76 @@ flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
 def main(_argv):
     physical_devices = tf.config.experimental.list_physical_devices('GPU')
     if len(physical_devices) > 0:
-        tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        for dev in physical_devices:
+            tf.config.experimental.set_memory_growth(dev, True)
 
-    trainset = Dataset(is_training=True, tiny=FLAGS.tiny)
-    testset = Dataset(is_training=False, tiny=FLAGS.tiny)
-    logdir = "./data/log"
-    isfreeze = False
-    steps_per_epoch = len(trainset)
-    first_stage_epochs = cfg.TRAIN.FISRT_STAGE_EPOCHS
-    second_stage_epochs = cfg.TRAIN.SECOND_STAGE_EPOCHS
-    global_steps = tf.Variable(1, trainable=False, dtype=tf.int64)
-    warmup_steps = cfg.TRAIN.WARMUP_EPOCHS * steps_per_epoch
-    total_steps = (first_stage_epochs + second_stage_epochs) * steps_per_epoch
+    strategy = tf.distribute.MirroredStrategy()
 
-    input_layer = tf.keras.layers.Input([cfg.TRAIN.INPUT_SIZE, cfg.TRAIN.INPUT_SIZE, 3])
-    NUM_CLASS = len(utils.read_class_names(cfg.YOLO.CLASSES))
-    STRIDES = np.array(cfg.YOLO.STRIDES)
-    IOU_LOSS_THRESH = cfg.YOLO.IOU_LOSS_THRESH
-    XYSCALE = cfg.YOLO.XYSCALE
-    ANCHORS = utils.get_anchors(cfg.YOLO.ANCHORS)
+    with strategy.scope():
+        input_layer = tf.keras.layers.Input([cfg.TRAIN.INPUT_SIZE, cfg.TRAIN.INPUT_SIZE, 3])
+        NUM_CLASS = len(utils.read_class_names(cfg.YOLO.CLASSES))
+        STRIDES = np.array(cfg.YOLO.STRIDES)
+        IOU_LOSS_THRESH = cfg.YOLO.IOU_LOSS_THRESH
+        XYSCALE = cfg.YOLO.XYSCALE
+        ANCHORS = utils.get_anchors(cfg.YOLO.ANCHORS)
 
-    freeze_layouts = ['conv2d_93', 'conv2d_101', 'conv2d_109']
+        freeze_layouts = ['conv2d_93', 'conv2d_101', 'conv2d_109']
 
-    if FLAGS.tiny:
-        freeze_layouts = ['conv2d_9', 'conv2d_12']
-        STRIDES = np.array(cfg.YOLO.STRIDES_TINY)
+        if FLAGS.tiny:
+            freeze_layouts = ['conv2d_9', 'conv2d_12']
+            STRIDES = np.array(cfg.YOLO.STRIDES_TINY)
 
-        feature_maps = YOLOv3_tiny(input_layer, NUM_CLASS)
-        bbox_tensors = []
-        for i, fm in enumerate(feature_maps):
-            bbox_tensor = decode_train(fm, NUM_CLASS, STRIDES, ANCHORS, i)
-            bbox_tensors.append(fm)
-            bbox_tensors.append(bbox_tensor)
-        model = tf.keras.Model(input_layer, bbox_tensors)
-    else:
-        if FLAGS.model == 'yolov3':
-            feature_maps = YOLOv3(input_layer, NUM_CLASS)
+            feature_maps = YOLOv3_tiny(input_layer, NUM_CLASS)
             bbox_tensors = []
             for i, fm in enumerate(feature_maps):
                 bbox_tensor = decode_train(fm, NUM_CLASS, STRIDES, ANCHORS, i)
                 bbox_tensors.append(fm)
                 bbox_tensors.append(bbox_tensor)
             model = tf.keras.Model(input_layer, bbox_tensors)
-        elif FLAGS.model == 'yolov4':
-            feature_maps = YOLOv4(input_layer, NUM_CLASS)
-            bbox_tensors = []
-            for i, fm in enumerate(feature_maps):
-                bbox_tensor = decode_train(fm, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE)
-                bbox_tensors.append(fm)
-                bbox_tensors.append(bbox_tensor)
-            model = tf.keras.Model(input_layer, bbox_tensors)
-
-    if FLAGS.weights == None:
-        print("Training from scratch")
-    else:
-        if FLAGS.weights.split(".")[len(FLAGS.weights.split(".")) - 1] == "weights":
-            if FLAGS.tiny:
-                utils.load_weights_tiny(model, FLAGS.weights)
-            else:
-                if FLAGS.model == 'yolov3':
-                    utils.load_weights_v3(model, FLAGS.weights)
-                else:
-                    utils.load_weights(model, FLAGS.weights)
         else:
-            model.load_weights(FLAGS.weights)
-        print('Restoring weights from: %s ... ' % FLAGS.weights)
+            if FLAGS.model == 'yolov3':
+                feature_maps = YOLOv3(input_layer, NUM_CLASS)
+                bbox_tensors = []
+                for i, fm in enumerate(feature_maps):
+                    bbox_tensor = decode_train(fm, NUM_CLASS, STRIDES, ANCHORS, i)
+                    bbox_tensors.append(fm)
+                    bbox_tensors.append(bbox_tensor)
+                model = tf.keras.Model(input_layer, bbox_tensors)
+            elif FLAGS.model == 'yolov4':
+                feature_maps = YOLOv4(input_layer, NUM_CLASS)
+                bbox_tensors = []
+                for i, fm in enumerate(feature_maps):
+                    bbox_tensor = decode_train(fm, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE)
+                    bbox_tensors.append(fm)
+                    bbox_tensors.append(bbox_tensor)
+                model = tf.keras.Model(input_layer, bbox_tensors)
 
-    optimizer = tf.keras.optimizers.Adam()
-    if os.path.exists(logdir): shutil.rmtree(logdir)
+        if FLAGS.weights == None:
+            print("Training from scratch")
+        else:
+            if FLAGS.weights.split(".")[len(FLAGS.weights.split(".")) - 1] == "weights":
+                if FLAGS.tiny:
+                    utils.load_weights_tiny(model, FLAGS.weights)
+                else:
+                    if FLAGS.model == 'yolov3':
+                        utils.load_weights_v3(model, FLAGS.weights)
+                    else:
+                        utils.load_weights(model, FLAGS.weights)
+            else:
+                model.load_weights(FLAGS.weights)
+            print('Restoring weights from: %s ... ' % FLAGS.weights)
+        optimizer = tf.keras.optimizers.Adam()
+
+    trainset = Dataset(is_training=True, tiny=FLAGS.tiny)
+
+    logdir = "./data/log"
+    isfreeze = False
+    first_stage_epochs = cfg.TRAIN.FISRT_STAGE_EPOCHS
+    second_stage_epochs = cfg.TRAIN.SECOND_STAGE_EPOCHS
+    global_steps = tf.Variable(1, trainable=False, dtype=tf.int64)
+
+    if os.path.exists(logdir):
+        shutil.rmtree(logdir)
     writer = tf.summary.create_file_writer(logdir)
 
     def train_step(image_data, target):
@@ -106,48 +108,15 @@ def main(_argv):
 
             gradients = tape.gradient(total_loss, model.trainable_variables)
             optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-            tf.print("=> STEP %4d   lr: %.6f   giou_loss: %4.2f   conf_loss: %4.2f   "
-                     "prob_loss: %4.2f   total_loss: %4.2f" % (global_steps, optimizer.lr.numpy(),
-                                                               giou_loss, conf_loss,
-                                                               prob_loss, total_loss))
-            # update learning rate
-            global_steps.assign_add(1)
-            if global_steps < warmup_steps:
-                lr = global_steps / warmup_steps * cfg.TRAIN.LR_INIT
-            else:
-                lr = cfg.TRAIN.LR_END + 0.5 * (cfg.TRAIN.LR_INIT - cfg.TRAIN.LR_END) * (
-                    (1 + tf.cos((global_steps - warmup_steps) / (total_steps - warmup_steps) * np.pi))
-                )
-            optimizer.lr.assign(lr.numpy())
+            return total_loss
 
-            # writing summary data
-            with writer.as_default():
-                tf.summary.scalar("lr", optimizer.lr, step=global_steps)
-                tf.summary.scalar("loss/total_loss", total_loss, step=global_steps)
-                tf.summary.scalar("loss/giou_loss", giou_loss, step=global_steps)
-                tf.summary.scalar("loss/conf_loss", conf_loss, step=global_steps)
-                tf.summary.scalar("loss/prob_loss", prob_loss, step=global_steps)
-            writer.flush()
-
-    def test_step(image_data, target):
-        with tf.GradientTape():
-            pred_result = model(image_data, training=True)
-            giou_loss = conf_loss = prob_loss = 0
-
-            # optimizing process
-            for i in range(len(freeze_layouts)):
-                conv, pred = pred_result[i * 2], pred_result[i * 2 + 1]
-                loss_items = compute_loss(pred, conv, target[i][0], target[i][1], STRIDES=STRIDES, NUM_CLASS=NUM_CLASS,
-                                          IOU_LOSS_THRESH=IOU_LOSS_THRESH, i=i)
-                giou_loss += loss_items[0]
-                conf_loss += loss_items[1]
-                prob_loss += loss_items[2]
-
-            total_loss = giou_loss + conf_loss + prob_loss
-
-            tf.print("=> TEST STEP %4d   giou_loss: %4.2f   conf_loss: %4.2f   "
-                     "prob_loss: %4.2f   total_loss: %4.2f" % (global_steps, giou_loss, conf_loss,
-                                                               prob_loss, total_loss))
+    @tf.function
+    def distributed_train_step(image_data, target):
+        per_replica_losses = strategy.run(train_step, args=(image_data, target))
+        total_loss = strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_losses,
+                                     axis=None)
+        global_steps.assign_add(1)
+        return total_loss
 
     for epoch in range(first_stage_epochs + second_stage_epochs):
         if epoch < first_stage_epochs:
@@ -163,11 +132,19 @@ def main(_argv):
                     freeze = model.get_layer(name)
                     unfreeze_all(freeze)
         for image_data, target in trainset:
-            train_step(image_data, target)
-        for image_data, target in testset:
-            test_step(image_data, target)
+            total_loss = distributed_train_step(image_data, target)
+            tf.print("=> STEP %4d   lr: %.6f   total_loss: %4.2f" % (global_steps, optimizer.lr.numpy(), total_loss))
+
+            # writing summary data
+            with writer.as_default():
+                tf.summary.scalar("lr", optimizer.lr, step=global_steps)
+                tf.summary.scalar("loss/total_loss", total_loss, step=global_steps)
+            writer.flush()
+
         if (epoch + 1) % 20 == 0:
-            model.save_weights(f"./checkpoints/yolov4_{epoch}.h5")
+            file_name = f"./checkpoints/yolov4_{epoch}.h5"
+            model.save_weights(file_name)
+            print(f"epoch {epoch + 1} saved at: {file_name}")
 
 
 if __name__ == '__main__':
